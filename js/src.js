1d4ae85ed35fc1ae325050ab6b6cb99f75ecddb4
Rob <robwalker@outlook.com>
2024-05-04 22:13:55 -0700

    change nav bar styling

--- js/src.js
+++ js/src.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
 	$("#btn-rawtext, .menu-link").hover(function() {
-		$(this).css("background-color", "#0D5E94");
+		$(this).css("background-color", "#FFF");
+		$(this).css("color", "#000");
 	},function() {
-		$(this).css("background-color", "#3593CE");
+		$(this).css("background-color", "#000");
+		$(this).css("color", "#fff");
 	});
 
 	$("#fldLeft, #fldMiddleT, #fldRight, #fldMiddleB").hover(function() {
